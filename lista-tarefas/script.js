d3ccc924b89b8482db261fef22aa3aece461fde0
João Mateus <joaomateuscvieira@gmail.com>
2025-01-17 19:25:54 -0300

    Ajustando script

--- lista-tarefas/script.js
+++ lista-tarefas/script.js
@@ -1,11 +1,29 @@
+function recuperarTasks() {
+    const tasksList = document.querySelector('#taskList');
+    const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+    
+    tasksList.innerHTML = '';
+    
+    tasks.forEach(task => {
+        const li = document.createElement('li');
+        li.textContent = task;
+        tasksList.appendChild(li);
+    });
+}
+
 const input = document.querySelector('#taskInput');
 
 document.querySelector('#addTaskButton').onclick = function() {
-    const task = input.value;
+    const task = input.value.trim();
 
-    if (task.trim() !== '') {
-        const lista = document.querySelector('.task-list');
-        lista.innerHTML += `<li>${task}</li>`;
-        input.value = ''; 
+    if (task !== '') {
+        const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+        tasks.push(task);
+        localStorage.setItem('tasks', JSON.stringify(tasks));
+
+        input.value = '';
+        recuperarTasks();
     }
 }
+
+window.onload = recuperarTasks;
