c05a20f3ca20517771efd5de90729ebf86f0c8f4
João Mateus <joaomateuscvieira@gmail.com>
2025-01-17 18:48:12 -0300

    Ajustando

--- lista-tarefas/script.js
+++ lista-tarefas/script.js
@@ -1,6 +1,10 @@
 document.querySelector('#addTaskButton').onclick = function() {
     const tarefas = document.querySelector('#taskInput').value
-    const lista = document.querySelector('#taskList');
+    const lista = document.querySelector('.task-list');
 
-    lista.textContent += tarefas;
+    if (tarefas === ''){
+        alert('Adicione alguma tarefa');
+    }
+
+    lista.innerHTML += `<li>${tarefas}</li>`;
 }
